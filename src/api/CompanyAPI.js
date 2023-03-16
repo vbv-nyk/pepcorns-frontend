9f78078e87a3fb912a9ce7ceb8968a2156ce2cb4
Vaibhav <vbv.nyk.2003@gmail.com>
2023-03-16 19:20:01 +0530

    Setup API skeleton

---
+++ src/api/CompanyAPI.js
@@ -0,0 +1,100 @@
+export const companies = [
+    {
+        "Company 1": {
+            main: {
+                logo: "https://api.pepcorns.com/clientassets/icon1677177731365.png",
+                title: "Company 1",
+                location: "Bangalore, Karnataka",
+                tags: ["Tag 1", "Tag 2"]
+            },
+            pitch: {
+                motto: "Stay Safe, Stay Strong!(ssssssss)",
+                websiteUrl: "https://www.google.com",
+                youtubeEmbed: "https://www.youtube.com/embed/l2e5FoOx3WE",
+                card: "LAKSFASDAS"
+            },
+            event: {
+                goal: 100000,
+                investors: 3,
+                length: 4,
+                dates: {
+                    start: "2023-03-20",
+                    end: "2023-03-25",
+                }
+            },
+            pitch: {
+                pdfUrl: "https://api.pepcorns.com/clientassets/icon1677185127750.pdf",
+                sections: [
+                    {
+                        name: "Highlights",
+                        title: "The existing meat supply ecosystem has multiple challenges",
+                        contents: ["Only profitable D2C brand in meats in India with a high return on capital efficiency.", "Backed by prolific angel investors like Amit Burman (Dabur chairman), SIDBI VC, Jitendra Gupta of Jupiter, Om Manchanda MD of Dr. Lal path lab, and many others.", "100% farm-integrated supply chain to bring transparency and traceability of fresh chicken, mutton, fish, and Seafood.", "Started by Deepanshu Manchanda who  conceptualising the category of meats by co-founding Licious the first D2C brand of meats", "Technology-enabled businesses focusing on end to end farm to fork"],
+                        imageURLs: ["https://ucarecdn.com/587800c6-340f-4330-a186-23fef8798de7/WemakeSkincareIntelligent101.png", "https://ucarecdn.com/16964edd-e771-4538-a964-fdc840d88882/WemakeSkincareIntelligent102.png", "https://ucarecdn.com/6b3443bf-ac22-41d4-b3ac-92639075c6df/WemakeSkincareIntelligent103.png"]
+                    }
+                ]
+            },
+            details: {
+                people: [{
+                    name: "Vaibhav Nayak",
+                    position: "College student",
+                    handles: {
+                        linkedin: "",
+                        facebook: "",
+                        twitter: "",
+                    }
+                }],
+                faq: [{
+                    question: "What is the exit possibility?",
+                    answer: "For CSOP holders Exit would be given in the next 3 years, while for SAFE investors would be given in IPO or sellout to a strategic player in the category.",
+                },
+                {
+                    question: "How many multipliers we can expect?",
+                    answer: "Over 2-3 years - 10x",
+                },
+                {
+                    question: "How are we different from the competition?",
+                    answer: "We are the only profitable D2C meat brand"
+                }]
+            },
+            discussions: [{
+                name: "User 1",
+                comment: "I like the motto"
+            }, {
+                name: "User 2",
+                comment: "This is a great initiative"
+            }
+            ],
+            updates: [{
+                date: "2023-02-24",
+                announcement: "AMA session 25th feb @5pm : See you all there!"
+            }, {
+                date: "2023-03-25",
+                announcement: "We would like to thank everyone for taking part in this, we hope to bring you happiness."
+            }],
+            documents: {
+                links: [
+                    {
+                        linkName: "Zip File With Documents",
+                        link: "https://api.pepcorns.com/clientassets/icon1677185115701.zip"
+                    }, {
+                        linkName: "Google Drive Link",
+                        link: "drive.google.com"
+                    }]
+            },
+            offers: [
+                {
+                    price: "1000",
+                    rewards: ["Be a part of our journey."],
+                    tnc: ["Community participants offer"],
+                    type: "CSOP"
+                },
+                {
+                    price: "5000",
+                    rewards: ["Be part of our growth sucess", "Special offer deals for you on our platform"],
+                    tnc: ["An exclusive subscription depends on your order volume.", "Just another condition"],
+                    type: "SAFE"
+                }
+            ]
+        },
+    }
+]
